f55b8b0b2b42770733a34e3ed2b451072f1894f9
Joakim Holm <joakimholm@protonmail.com>
2023-01-14 13:45:23 +0100

    Moved all source definitions to sources/__init__.py

--- audiobookdl/sources/__init__.py
+++ audiobookdl/sources/__init__.py
@@ -1,10 +1,21 @@
-from .sources import *
-
+from .audiobooksdotcom import AudiobooksdotcomSource
+from .chapter import ChapterSource
+from .chirp import ChirpSource
+from .ereolen import EreolenSource
+from .librivox import LibrivoxSource
+from .overdrive import OverdriveSource
+from .scribd import ScribdSource
+from .yourcloudlibrary import YourCloudLibrarySource
 
 def get_source_classes():
     """Returns a list of all available sources"""
     return [
-        klass
-        for name, klass in globals().items()
-        if name.endswith('Source')
+        AudiobooksdotcomSource,
+        ChapterSource,
+        ChirpSource,
+        EreolenSource,
+        LibrivoxSource,
+        OverdriveSource,
+        ScribdSource,
+        YourCloudLibrarySource,
     ]
