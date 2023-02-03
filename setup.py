907c5bec85d45b762b6e4849063bde98a4cff94b
Joakim Holm <joakimholm@protonmail.com>
2023-02-03 19:21:35 +0100

    Change version to 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="audiobook-dl",
-    version="0.4.0",
+    version="0.4.1",
     packages=find_packages(),
     description="Downloads audiobooks",
     install_requires=[
