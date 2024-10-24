6092145e930b59799e2c34bdc34078bd81099c8a
Simon Yde <git@simonyde.com>
2024-10-24 22:23:42 +0200

    feat(nix): add flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "audiobook-dl flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = self.packages.${system}.audiobook-dl;
+        packages.audiobook-dl = pkgs.callPackage ./. { inherit self; };
+      }
+    );
+}
